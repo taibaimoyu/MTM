9bdd0c2ea7f9f4443dcf8f0e21468b02cf5436ec
taibaimoyu <1016794862@qq.com>
2018-08-30 15:26:59 +0800

    add

---
+++ src/main/java/com/mtm/MtmApplication.java
@@ -0,0 +1,12 @@
+package com.mtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtmApplication.class, args);
+	}
+}
